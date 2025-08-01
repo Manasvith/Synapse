e6061a7637cdaad76c0a081b208293005ae0469e
Stark <Stark>
2025-08-01 15:54:52 +0000

    Add server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import { addHoldingsController } from './contollers/addHoldingsController.js'
+
+const PORT = 5001
+const app = express()
+
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5001',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+
+app.post('/addHoldings', addHoldingsController.addHoldings)
+
+app.use((err, req, res, next) => {
+    console.error('Error:', err.stack);
+    res.status(500).json({ 
+        success: false, 
+        error: 'Something went wrong!' 
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
