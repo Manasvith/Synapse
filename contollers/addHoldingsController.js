f5a314406c865b10809ea2131b2de8da253c4ea5
Stark <stark13129@gmail.com>
2025-08-01 16:01:13 +0000

    Add feature Add Holdings

---
+++ contollers/addHoldingsController.js
@@ -0,0 +1,45 @@
+import { connection } from '../db.js'
+
+export const addHoldingsController = {
+    addHoldings: async (request, response) => {
+      try {
+        const [rows] = await connection.query(
+          'SELECT * FROM portfolio WHERE company = ?',
+          [request.body.company]
+        );
+  
+        let alreadyHeld = false
+        let holding_price = 0
+        let holding_quantity = 0
+  
+        if (rows && rows.length > 0) {
+          alreadyHeld = true
+          holding_price = rows[0].avg_price
+          holding_quantity = rows[0].quantity
+        }
+        
+        console.log(request.body.company, alreadyHeld, holding_price, holding_price)
+
+        if (!alreadyHeld) {
+          const query = 'INSERT INTO portfolio (company, quantity, avg_price, time_stamp) VALUES (?, ?, ?, ?)'
+          const values = [request.body.company, request.body.quantity, request.body.price, request.body.timestamp]
+  
+          const [result] = await connection.query(query, values)
+          return response.status(200).json({ success: true, data: result })
+        } else {
+          const newQuantity = holding_quantity + request.body.quantity
+          const newAvgPrice =
+            ((holding_price * holding_quantity) + (request.body.price * request.body.quantity)) / newQuantity
+
+          const query = 'UPDATE portfolio SET quantity = ?, avg_price = ?, time_stamp = ? WHERE company = ?'
+          const values = [newQuantity, newAvgPrice, request.body.timestamp, request.body.company]
+  
+          const [result] = await connection.query(query, values);
+          return response.status(200).json({ success: true, message: "Inserted successfully" });
+        }
+      } catch (error) {
+        console.error('Error adding holdings:', error);
+        return response.status(500).json({ success: false, message: error.message });
+      }
+    }
+  };  
