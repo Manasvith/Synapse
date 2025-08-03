eedd70027fb0e6983e91c8f2178fa7146de33196
Stark <stark13129@gmail.com>
2025-08-03 18:46:07 +0000

    Add removeAllHoldings

---
+++ controllers/removeAllHoldingsController.js
@@ -0,0 +1,24 @@
+import { connection } from '../db.js'
+
+export const removeAllHoldingsController = {
+    removeAllHoldings : async(request, response) => {
+        try {
+            let company = request.body.company
+            let [rows] =await connection.query('SELECT * FROM portfolio WHERE company = ?', company)
+            
+            let allHoldings = rows[0].quantity
+
+            const query = 'DELETE FROM portfolio where company = ?'
+            await connection.query(query, company)
+
+            const que = 'INSERT INTO transactions (company, action, price, quantity, time_stamp) VALUES(?, ?, ?, ?, ?)';
+            const vals = [company, 'sell', request.body.price, allHoldings, request.body.timestamp]
+            await connection.query(que, vals);
+
+            return response.status(200).json({ success: true, message: "All holdings removed successfully" });
+        } catch (error) {
+            console.error('Error removing all holdings:', error);
+            return response.status(500).json({ success: false, message: error.message });
+        }
+    }
+}
