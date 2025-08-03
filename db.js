7bc23c1f337a8dc5781a284aa98ee4c89ab627cc
Stark <stark13129@gmail.com>
2025-08-03 16:31:37 +0000

    Add db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const connection = await mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD
+});
