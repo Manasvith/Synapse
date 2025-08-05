a3ecc65b3816fa2efec7bceae8940f17e9045417
Stark <stark13129@gmail.com>
2025-08-05 06:35:17 +0000

    Modify addToSettlementAccount

--- controllers/addToSettlementAcctController.js
+++ controllers/addToSettlementAcctController.js
@@ -4,6 +4,10 @@ export const addToSettlementAcctController = {
     addToSettlementAccount: async (request, response) => {
         try {
 
+            if(typeof(request.body.transaction_amount) !== 'number') {
+                return response.status(400).json({success: "false", message: "Bad request: No amount specified"})
+            }
+
             const [r] = await connection.query("SELECT * FROM settlementaccount ORDER BY time_stamp DESC LIMIT 1")
             let curr_balance = r[0].current_balance
 
