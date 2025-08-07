764baf30aac71e02e30a9de5a61d3b4f704a27b8
Stark <stark13129@gmail.com>
2025-08-07 08:25:42 +0000

    modify addHoldings

--- controllers/addHoldingsController.js
+++ controllers/addHoldingsController.js
@@ -3,8 +3,8 @@ import { connection } from '../db.js'
 export const addHoldingsController = {
     addHoldings: async (request, response) => {
       try {
-        if(request.body.quantity <= 0) {
-          return response.status(400).json({ success: false, message: "Bad request: Quantity must be greater than zero" });
+        if(request.body.quantity <= 0 || typeof(request.body.quantity) != 'number') {
+          return response.status(400).json({ success: false, message: "Bad request: Quantity must be a number greater than zero" });
         }
         
         const [rows] = await connection.query(
